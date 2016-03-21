438d80a1dc6e45f339328181ce1659d628b49823
Vishal Rathi <vishalkumar95@users.noreply.github.com>
2016-03-20 23:52:31 -0400

    Implemented the autokey Vigenere Cipher which uses the ciphertext as a key

---
+++ Vigenere_Cipher_Autokey_Plaintext.py
@@ -0,0 +1,39 @@
+# This function is an implementation of the autokey Vigenere cipher algorithm.
+
+def decryptVigenere(ciphertext, key):
+
+    ciphertextbreak = []
+    ciphertextbreakextra = []
+    Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    smallLetters = 'abcdefghijklmnopqrstuvwxyz'
+    keylength = len(key)
+    ciphertextlength = len(ciphertext)
+    diff = ciphertextlength - keylength
+    finalplaintext = list('.' * ciphertextlength)
+
+    ciphertextbreak.append(ciphertext[0 : keylength])
+    ciphertextbreakextra.append(ciphertext[-diff:])
+
+    for i in range(0, len(ciphertextbreak)):
+        for j in range(0, keylength):
+            indkey = smallLetters.index(key[j])
+            indcipher = Letters.index(ciphertextbreak[i][j])
+            newindcipher = indcipher - indkey
+            modindcipher = newindcipher % 26
+            finalplaintext[j] = smallLetters[modindcipher]
+
+    for i in range(0, diff):
+        indkey = smallLetters.index(finalplaintext[i])
+        indcipher = Letters.index(ciphertextbreakextra[0][i])
+        newindcipher = indcipher - indkey
+        modindcipher = newindcipher % 26
+        finalplaintext[keylength + i] = smallLetters[modindcipher]
+
+    plaintext = "".join(finalplaintext)
+    plaintext = plaintext.upper()
+    print(plaintext)
+        
+
+                                            
+        
+    
